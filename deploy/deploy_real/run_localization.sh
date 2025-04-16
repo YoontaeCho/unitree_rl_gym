365e10739985670a462d695473497979b703a60a
HYUNHONOH98 <hhnoh98@naver.com>
2025-04-16 17:29:33 +0900

    use state pub from origin

--- deploy/deploy_real/run_localization.sh
+++ deploy/deploy_real/run_localization.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
-python3 localization/state_pub.py &
-ros2 launch localization/state_pub.launch.py &
+python3 state_pub.py &
+ros2 launch state_pub.launch.py &
 ros2 launch livox_ros_driver2 msg_MID360_launch.py &
 ros2 launch fast_lio mapping.launch.py config_file:=mid360.yaml rviz:=False &
 python3 localization/pelvis_pub_v2.py &
