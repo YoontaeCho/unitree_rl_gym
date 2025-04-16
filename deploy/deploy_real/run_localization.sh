e679cccc1cda29b0edf724f2df9ef9aa5ff5ad12
HYUNHONOH98 <hhnoh98@naver.com>
2025-04-16 17:01:48 +0900

    dbg

--- deploy/deploy_real/run_localization.sh
+++ deploy/deploy_real/run_localization.sh
@@ -3,6 +3,6 @@ python3 localization/state_pub.py &
 ros2 launch localization/state_pub.launch.py &
 ros2 launch livox_ros_driver2 msg_MID360_launch.py &
 ros2 launch fast_lio mapping.launch.py config_file:=mid360.yaml rviz:=False &
-python3 localization/pelvis_pub.py &
+python3 localization/pelvis_pub_v2.py &
 # python3 fake_world_tf_pub.py &
 ros2 run rviz2 rviz2
